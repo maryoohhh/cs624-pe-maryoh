10a052a27d4e287a190e4c0a5050723ada36d423
Mary Oh <marychristineoh@gmail.com>
2023-04-20 20:59:25 +0000

    PE03-todos submission

--- PE03-todos/App.js
+++ PE03-todos/App.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 import Input from './Input';
 import Heading from './Heading';
+import Button from './Button';
+import TodoList from './TodoList';
+import TabBar from './TabBar';
+
+let todoIndex = 0
 
 class App extends Component {
   constructor(){
@@ -10,7 +15,11 @@ class App extends Component {
       inputValue: '',
       todos: [],
       type: 'All',
-    };
+    }
+    this.submitTodo = this.submitTodo.bind(this)
+    this.toggleComplete = this.toggleComplete.bind(this)
+    this.deleteTodo = this.deleteTodo.bind(this)
+    this.setType = this.setType.bind(this)
   }
 
   inputChange(inputValue){
@@ -18,17 +27,62 @@ class App extends Component {
     this.setState({inputValue});
   }
 
+  submitTodo () {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return
+    }
+    const todo = {
+      title: this.state.inputValue,
+      todoIndex,
+      complete: false
+    }
+    todoIndex++
+    const todos = [...this.state.todos, todo]
+    this.setState({todos, inputValue: ''}, () => {
+      console.log('State: ', this.state)
+    })
+  }
+
+  deleteTodo (todoIndex){
+    let {todos} = this.state
+    todos = todos.filter((todo) => todo.todoIndex !== todoIndex)
+    this.setState({todos})
+  }
+
+  toggleComplete(todoIndex){
+    let todos = this.state.todos
+    todos.forEach((todo) => {
+      if (todo.todoIndex === todoIndex) {
+        todo.complete = !todo.complete
+      }
+    })
+    this.setState({todos})
+  }
+
+  setType (type) {
+    this.setState({type})
+  }
+
   render() {
-    const {inputValue} = this.state;
+    const {inputValue, todos, type} = this.state;
     return(
       <View style = {styles.container}>
-        <ScrollView keyboardShouldPersistTaps = "always" style = {styles.content}>
+        <ScrollView
+          keyboardShouldPersistTaps = "always"
+          style = {styles.content}>
           <Heading />
           <Input
             inputValue = {inputValue}
-            inputChange = {text => this.inputChange(text)}
+            inputChange = {(text) => this.inputChange(text)}
           />
+          <TodoList
+            type = {type}
+            toggleComplete = {this.toggleComplete}
+            deleteTodo = {this.deleteTodo}
+            todos = {todos} />
+          <Button submitTodo = {this.submitTodo} />
         </ScrollView>
+        <TabBar type = {type} setType = {this.setType} />
       </View>
     );
   }
